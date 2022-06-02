86661b5b371e6d5aa5992b23df1f9cc3c09fd52a
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-06-02 20:05:21 +0530

    initial code template setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='underline text-4xl'>
+        Testing
+    </div>
+  )
+}
+
+export default App
