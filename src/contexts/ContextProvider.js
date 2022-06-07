21009fd742674d75334f6448c1230d44b4621481
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-06-07 19:58:20 +0530

    create Navbar & responsive Sidebar

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -11,9 +11,22 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
 	const [activeMenu, setActiveMenu] = useState(true)
-
+	const [isClicked, setIsClicked] = useState(initialState)
+	const handleClick = (clicked) => {
+		setIsClicked({ ...initialState, [clicked]: true })
+	}
+	const [screenSize, setScreenSize] = useState(undefined)
 	return (
-		<StateContext.Provider value={{ activeMenu, setActiveMenu }}>
+		<StateContext.Provider
+			value={{
+				activeMenu,
+				setActiveMenu,
+				isClicked,
+				setIsClicked,
+				handleClick,
+				screenSize,
+				setScreenSize,
+			}}>
 			{children}
 		</StateContext.Provider>
 	)
