1a073e312fe4e1b598714e81d701637ffa51d2ef
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-06-08 03:29:42 +0530

    Homepage sparkline chart added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react"
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
+const Button = ({ bgColor, color, text, size, borderRadius }) => {
+	return (
+		<button
+			type="button"
+			style={{ backgroundColor: bgColor, color, borderRadius }}
+			className={`text-${size} p-3 hover:drop-shadow-xl`}>
+			{text}
+		</button>
+	)
 }
 
 export default Button
