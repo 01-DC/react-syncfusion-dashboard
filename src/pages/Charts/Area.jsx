2dab4ed2693eb0c31f4fe4e5dc2613676f0abd89
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-06-08 16:03:01 +0530

    create Area Chart page

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react"
+import {
+	ChartComponent,
+	SeriesCollectionDirective,
+	SeriesDirective,
+	Inject,
+	DateTime,
+	Legend,
+	SplineAreaSeries,
+} from "@syncfusion/ej2-react-charts"
+
+import { Header } from "../../components"
+import {
+	areaCustomSeries,
+	areaPrimaryYAxis,
+	areaPrimaryXAxis,
+} from "../../data/dummy"
 
 const Area = () => {
-  return (
-    <div>Area</div>
-  )
+	return (
+		<div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+			<Header category="Area" title="Inflation Rate in Percentage" />
+			<div className="w-full">
+				<ChartComponent
+					id="area-chart"
+					height="420px"
+					primaryXAxis={areaPrimaryXAxis}
+					primaryYAxis={areaPrimaryYAxis}
+					chartArea={{ border: { width: 0 } }}
+					tooltip={{ enabled: true }}>
+					<Inject services={[SplineAreaSeries, DateTime, Legend]} />
+					<SeriesCollectionDirective>
+						{areaCustomSeries.map((item, index) => (
+							<SeriesDirective key={index} {...item} />
+						))}
+					</SeriesCollectionDirective>
+				</ChartComponent>
+			</div>
+		</div>
+	)
 }
 
 export default Area
